356a25f91a6e9d84e8d067feb83129eee0b289e3
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-09-03 17:25:43 +0530

    Changes

--- src/DataLayer.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useReducer, useContext } from 'react';
-
-export const DataLayerContext = createContext();
-
-export const DataLayer = ({ initialState, reducer, children }) => (
-    <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
-        {children}
-    </DataLayerContext.Provider>
-)
