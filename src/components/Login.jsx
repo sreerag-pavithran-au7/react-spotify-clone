90cc42980b5ceb7d8518babf7c32e9a6744c8ca2
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-09-03 09:38:30 +0530

    Added Basic API

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/Spotify-Logo-png-RGB-White-750x225.png" 
+            alt="Logo" />
+        <a href="https://">LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/components/Spotify.js
@@ -0,0 +1,13 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const redirectUri = "https://localhost:3000/";
+
+export const clientId = "61fc535165864900a5691aa664e9232c";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
